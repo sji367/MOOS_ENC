6539f7cebf7027cdf846d3e0df338876f4670bdf
sji367 <sji367@wildcats.unh.edu>
2016-06-20 16:48:18 -0400

    Added polygon search algorithm
    The search algorithm works relatively well. However, it doesn't work in boundary cases (when the angle goes from 359 to 0) and when the polygon is concave and you are in the "dent".

--- ENC_Search.py
+++ ENC_Search.py
@@ -17,13 +17,17 @@ import pyproj
 # GDAL is a library that reads and writes shapefiles
 from osgeo import ogr
 
-#==============================================================================
-# This program uses the X and Y cooridinates from the ASV and filters out all 
-#   of the points from the ENC database that are in a predetermined search
-#   radius. It then outputs the 
-#==============================================================================
+# Numpy is a useful tool for 
+import numpy as np
 
 comms = pymoos.comms()
+
+# Calculate the origin 
+p = pyproj.Proj(proj='utm', zone=19, ellps='WGS84')
+
+LatOrigin  = 43.071959194444446
+LongOrigin = -70.711610833333339 
+x_origin,y_origin = p(LongOrigin, LatOrigin)
 
 # Register for updates of the MOOS variables NAV_X and NAV_Y once every second
 def on_connect():
@@ -31,16 +35,108 @@ def on_connect():
     comms.register('NAV_Y',1)
     comms.register('NAV_HEADING',1)
     return True
-
+#==============================================================================
+# Function to determine what the important information should be outputed for
+#   each polygon in the search area. This function only works if there are 
+#   vertices of the polygon in the search area.
+#==============================================================================
+def polygon(ASV_X,ASV_Y, heading, feature, intersect, TF_intersect, cntr):
+    if (TF_intersect):
+        geom = intersect
+    else:
+        geom = feature.GetGeometryRef()
+        
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    x_small = 9999
+    y_small = 9999
+    
+    min_angle = 360
+    max_angle = 0
+    min_dist = 9999 
+    min_dist_angle = 999
+    
+    d_min = 0
+    d_max = 0
+    min_dist = 9999
+    
+    ring = geom.GetGeometryRef(0)
+    num_points =  ring.GetPointCount()
+    # Cycle through to find the matching x and y coordinates
+    for i in range (0, num_points):
+        lon = ring.GetX(i)#np.around(ring.GetX(i), decimals=6)
+        lat = ring.GetY(i)#np.around(ring.GetY(i), decimals=6)
+        ptx, pty = p(lon, lat)
+        
+        ptx += -x_origin
+        pty += -y_origin
+        
+        d = np.sqrt(np.square(ASV_X-ptx)+np.square(ASV_Y-pty))
+        angle = np.arctan2(pty-ASV_Y,ptx-ASV_X)*180/np.pi      
+        
+        # Make sure that the angle are positive
+        if angle <0:
+            angle += 360
+        angle += heading
+        
+        # Make sure that the angle are below 360
+        if angle >360:
+            angle += -360
+            
+        # Determine if the angle is the minimum angle
+        if angle < min_angle:
+            min_angle = angle
+            d_min = d
+            x1 = ptx
+            y1 = pty
+        
+        # Determine if the angle is the maximum angle
+        if angle > max_angle:
+            max_angle = angle
+            d_max = d
+            x2 = ptx
+            y2 = pty
+        
+        # Check if this point is the points match the envelope and to find  
+        #   the x and y coordinates is the shortest distance
+        if min_dist > d:
+            min_dist = d
+#            print "distance: %f" %d
+            min_dist_angle = angle
+            x_small = ptx
+            y_small = pty
+        
+    # Post these points to pMarineViewer
+    pt1 = 'x='+str(x1)+',y='+str(y1)+',vertex_size=8,vertex_color=white,label=pt1_'+str(cntr)
+    comms.notify('VIEW_POINT', pt1, pymoos.time())
+    time.sleep(.001)
+    pt2 = 'x='+str(x2)+',y='+str(y2)+',vertex_size=8,vertex_color=white,label=pt2_'+str(cntr)
+    comms.notify('VIEW_POINT', pt2, pymoos.time())
+    
+    # Find other useful info on the obstacle
+    t_lvl = feature.GetField(0)
+    obs_type = feature.GetField(1)
+    
+    if num_points:
+    # t_lvl,type @ min_ang,max_ang,min_dist_ang @ min_ang_dist,max_ang_dist,min_dist
+    #   To convert the calculated angle to the one that relates to the one that
+    #   MOOS outputs, you have to use the formula: MOOS_ang = -(calc_ang-90)
+        poly = str(t_lvl)+','+str(obs_type)+'@'+str(np.mod(-(max_angle-heading)+90, 360))+','+str(np.mod(-(min_angle-heading)+90, 360))+','+str(np.mod(-(min_dist_angle-heading)+90, 360))+'@'+str(d_min)+','+str(d_max)+','+str(min_dist)
+    else:
+        poly = "No points"    
+    
+    return poly
+            
+#==============================================================================
+# This program uses the X and Y cooridinates from the ASV and filters out all 
+#   of the points from the ENC database that are in a predetermined search
+#   radius. It then outputs information need for obstacle avoidance to the
+#   MOOSDB as a string.
+#==============================================================================
 def main():
-    t = 0#pymoos.time()
     
-    # Calculate the origin 
-    p = pyproj.Proj(proj='utm', zone=19, ellps='WGS84')
-    
-    LatOrigin  = 43.071959194444446
-    LongOrigin = -70.711610833333339 
-    x_origin,y_origin = p(LongOrigin, LatOrigin)
 
     #easily mark all of the output and input files
     file_pnt = '/home/mapper/Desktop/MOOS_ENC/Data/US5NH02M/Shape/ENC_pnt2.shp'  
@@ -85,7 +181,8 @@ def main():
             #   spatial filter
             layer.SetSpatialFilter(None)
             layer_poly.SetSpatialFilter(None)
-            ring_filter = poly_filter = None
+            ring_filter = None
+            poly_filter = None
                 
             # Create the baseline for the search area polygon 
             ring_filter = ogr.Geometry(ogr.wkbLinearRing)
@@ -96,9 +193,13 @@ def main():
             X = x_norm+x_origin
             Y = y_norm+y_origin
             
+            # Search area should be a function of the vessel size and current 
+            #   speed
+            search_dis = 50            
+            
             # Convert the positions to latitude and longitude
-            E_long_search,N_lat_search= p(X+50, Y+50, inverse=True)
-            W_long_search,S_lat_search= p(X-50, Y-50, inverse=True)
+            E_long_search,N_lat_search= p(X+search_dis, Y+search_dis, inverse=True)
+            W_long_search,S_lat_search= p(X-search_dis, Y-search_dis, inverse=True)
 #            print 'N_lat: %f, E_long: %f' %(N_lat_search, E_long_search)
 #            print 'S_lat: %f, W_long: %f' %(S_lat_search, W_long_search)
             
@@ -111,16 +212,17 @@ def main():
             poly_filter.AddGeometry(ring_filter) # Add the ring to the previously created polygon         
             
             # Show the Search Radius Polygon on pMarnineViewer
-            s_poly_vert = 'pts={'+str(x_norm-50)+','+ str(y_norm+50)+':'+ str(x_norm+50)+','+str(y_norm+50)+':'+ str(x_norm+50)+','+str(y_norm-50)+':' +str(x_norm-50)+','+str(y_norm-50)+'},'
-            comms.notify('VIEW_POLYGON', s_poly_vert+'label=Search,edge_size=10,vertex_size=1,edge_color=red', pymoos.time()+t)
+            s_poly_vert = 'pts={'+str(x_norm-search_dis)+','+ str(y_norm+search_dis)+':'+ str(x_norm+search_dis)+','+str(y_norm+search_dis)+':'+ str(x_norm+search_dis)+','+str(y_norm-search_dis)+':' +str(x_norm-search_dis)+','+str(y_norm-search_dis)+'},'
+            comms.notify('VIEW_POLYGON', s_poly_vert+'label=Search,edge_size=10,vertex_size=1,edge_color=red', pymoos.time())
             # Filter out data to determine the search area and print out the 
             #   number of potential hazards in the imediate vacinity 
-            layer.SetAttributeFilter("t_lvl!=0")
+            feature_poly = layer_poly.GetFeature(1)
             layer.SetSpatialFilter(poly_filter)
-            layer_poly.SetAttributeFilter("t_lvl!=0")
+            layer.SetAttributeFilter("t_lvl!=0")
             layer_poly.SetSpatialFilter(poly_filter)
+            layer_poly.SetAttributeFilter("t_lvl!=0")
             feature = layer.GetNextFeature()
-            feature_poly = layer_poly.GetNextFeature()
+#            feature_poly = layer_poly.GetFeature(0)
             print 'Number of Hazards (point): %d' %layer.GetFeatureCount()
             print 'Number of Hazards (polygon): %d \n' %layer_poly.GetFeatureCount()
             
@@ -135,9 +237,14 @@ def main():
             
             # Intialize the obstacle string
             obs_pos = ''
+            
+###############################################################################
+##########             Cycle through the point obstacles             ##########
+###############################################################################
             # Highlight all point features in search radius in pMarineViewer 
             #   and store them into a MOOS variable called "Obstacles"
-            for i in range(layer.GetFeatureCount()):
+            feature = layer.GetNextFeature() 
+            while feature:
                 time.sleep(.001)
                 geom_point = feature.GetGeometryRef()
                 x,y = p(geom_point.GetX(), geom_point.GetY())
@@ -145,12 +252,12 @@ def main():
                 new_y = y-y_origin
                 pos = 'x='+str(new_x)+',y='+str(new_y)
                 poly_search = 'format=radial,'+pos+',radius=25,pts=8,edge_size=5,vertex_size=2,edge_color=aquamarine,vertex_color=aquamarine,label='+str(counter)
-                comms.notify('VIEW_POLYGON', poly_search, pymoos.time()+t)
+                comms.notify('VIEW_POLYGON', poly_search, pymoos.time())
                                 
                 # Store information for the obstacle to be used later to post to the MOOSDB
                 # x,y,t_lvl,type 
                 if feature.GetField(0) !=0:
-                    # if it isnt the last obstacle put a ! at the end
+                    # if it isnt the first obstacle put a ! at the end
                     if num_obs!=0:
                         obs_pos += '!'
                     num_obs += 1
@@ -159,57 +266,88 @@ def main():
                 # Go to the next feature and increment the counter
                 feature = layer.GetNextFeature() 
                 counter += 1
+            #print 'Number of Hazards (point): %d, %d' %(layer.GetFeatureCount(), num_obs)
             # Output to the MOOSDB a list of obstacles
             #   ASV_X,ASV_Y : # of Obstacles : x,y,t_lvl,type : x,y,t_lvl,type : ...
 #            if num_obs != 0:
-            obstacles = str(x_norm)+','+str(y_norm)+','+str(heading)+':'+str(num_obs)+':'+obs_pos        
-            comms.notify('Obstacles', obstacles, pymoos.time()+t)            
+            obstacles = str(x_norm)+','+str(y_norm)+','+str(heading)+':'+str(num_obs)+':'+obs_pos
+            comms.notify('Obstacles', obstacles, pymoos.time())            
             # Determine if a new polygon was used
             if max_cntr < counter:
-                max_cntr = counter
-
-            ## Cycle throught the polygons 
-#            while feature_poly:
-#                geom_poly = feature_poly.GetGeometryRef() # Polygon from shapefile
-#                # Get the interesection of the polygon from the shapefile and
-#                #   the outline of tiff from pMarnineViewer
-#                intersection_poly = geom_poly.Intersection(poly_filter) 
-#            
-#                # Get the ring of that intersection polygon
-#                p_ring = intersection_poly.GetGeometryRef(0) 
-#        
-#                # Determine how many vertices there are in the polygon
-#                points = p_ring.GetPointCount()
-#                vertex = 'pts={' # String to hold the vertices
-#                # Cycle through the vertices and store them as a string
-#                for p1 in xrange(points):
-#                    lon, lat, z = p_ring.GetPoint(p1)
-#                    p_x,p_y = p(lon, lat)
-#                    vertex += str(p_x-x_origin) + ','+ str(p_y-y_origin)
-#                    if (p1!=points-1):
-#                        vertex += ':'
-#                poly = vertex+'},edge_color=yellow,label='+str(counter_poly)
-#                if points !=0:
-#                    comms.notify('VIEW_SEGLIST', poly, pymoos.time()+t)
-#                counter_poly += 1
-#                feature_poly = layer_poly.GetNextFeature()
-#                
-#            # Determine if a new polygon was used
-#            if max_cntr_poly < counter_poly:
-#                max_cntr_poly = counter_poly    
-            # Move polygon off screen - temperary fix for removing highlighted 
-            #   obstacles (shown as polygons) from pMarineViewer
-            for i in range(counter, max_cntr+1):
+                max_cntr = counter  
+                
+            # Remove highlighted point obstacles (shown as polygons) from 
+            #   pMarineViewer
+            for i in range(counter, max_cntr):
                 time.sleep(.002)
-                poly = 'format=radial,x= 5000,y=5000,radius=25,pts=8,edge_size=5,vertex_size=2,label='+str(i)
-                comms.notify('VIEW_POLYGON', poly, pymoos.time()+t)
-            
-#            # Move polygon off screen - temperary fix for removing highlighted 
-#            #   obstacles (shown as polygons) from pMarineViewer
-#            for i in range(counter_poly, max_cntr_poly+1):
-#                time.sleep(.002)
-#                poly = 'pts={5000,5000: 5001,5001},label='+str(i)
-#                comms.notify('VIEW_SEGLIST', poly, pymoos.time()+t)
+                poly = 'format=radial,x= 0,y=0,radius=25,pts=8,edge_size=5,vertex_size=2,active=false,label='+str(i)
+                comms.notify('VIEW_POLYGON', poly, pymoos.time())
+                
+###############################################################################
+##########                 Cycle through the polygons                ##########
+###############################################################################
+            # Initialize the polygon strings
+            poly_str = ''
+            poly_info = ''
+            feature_poly = layer_poly.GetNextFeature()
+            while feature_poly:
+                geom_poly = feature_poly.GetGeometryRef() # Polygon from shapefile
+                # Get the interesection of the polygon from the shapefile and
+                #   the outline of tiff from pMarnineViewer
+                intersection_poly = geom_poly.Intersection(poly_filter) 
+                
+                # Get the ring of that intersection polygon
+                p_ring = intersection_poly.GetGeometryRef(0) 
+                
+                # There are two potential cases - There are vertices of the 
+                #   polygon within the search area and There are no vertices of
+                #   the polygon within the search area.
+                
+                # Case 1: There are vertices within the search area therefore 
+                #   we will give the polygon string function the intersection
+                #   of the polygon and the search area
+                if p_ring:
+                    poly_str = polygon(x_norm, y_norm, heading, feature_poly, intersection_poly, True,counter_poly)
+                    
+                # Case 2: there are no points in the the search window, 
+                #   therefore we are temperarily giving the entire polygon to 
+                #   the polygon function.
+                else:
+                    poly_str = polygon(x_norm, y_norm, heading, feature_poly, False, False, counter_poly)
+   
+                # If it is not the first polygon, then add a '!' to the end of 
+                #   the  string.
+                if poly_str!="No points":
+                    if counter_poly==1:
+                        poly_info = poly_str
+                    elif counter_poly>1:
+                        poly_info += '!'
+                        poly_info += poly_str
+                    # Increment counter
+                    counter_poly += 1
+                    
+                # Go to the next polygon
+                feature_poly = layer_poly.GetNextFeature()
+                
+
+            # Post an update if there are polygon obstacles
+            if (layer_poly.GetFeatureCount()>1):
+                poly_obs = str(x_norm)+','+str(y_norm)+','+str(heading)+':'+str(layer_poly.GetFeatureCount()-1)+':'+poly_info
+                comms.notify('Poly_Obs', poly_obs, pymoos.time())  
+            else:
+                poly_obs = str(x_norm)+','+str(y_norm)+','+str(heading)+':'+str(layer_poly.GetFeatureCount()-1)
+                comms.notify('Poly_Obs', poly_obs, pymoos.time())
+            # Determine if a new polygon was used
+            if max_cntr_poly < counter_poly:
+                max_cntr_poly = counter_poly    
+                
+            # Remove highlighted line/polygon obstacles (shown as seglist) from 
+            #   pMarineViewer
+            for i in range(1, counter_poly):
+                time.sleep(.002)
+                poly = 'x=1,y1,active=false,label=pt1_'+str(i)
+                poly = 'x=1,y1,active=false,label=pt2_'+str(i)
+                comms.notify('VIEW_POINT', poly, pymoos.time())
                 
             
    
@@ -217,7 +355,7 @@ def main():
         #   variable to avoid this problem if nothing was posted during the l
         #   last cycle
         else:
-            comms.notify('dummy_var','',pymoos.time()+t)
+            comms.notify('dummy_var','',pymoos.time())
 
 if __name__ == "__main__":
     main()        
